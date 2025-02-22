5edf69c4de09448be6eb23c5fa42676b89729351
TakeItEasy <semen.herasymiuk@gmail.com>
2025-02-22 19:08:41 +0600

    hw react 01

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,19 @@
+import FriendListItem from "./FriendListItem";
+import styles from "./FriendList.module.css";
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={styles.friendList}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem 
+          key={id} 
+          avatar={avatar} 
+          name={name} 
+          isOnline={isOnline} 
+        />
+      ))}
+    </ul>  
+  );
+};
+
+export default FriendList;
---
+++ src/components/FriendList/FriendListItem.jsx
@@ -0,0 +1,15 @@
+import styles from "./FriendList.module.css";
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className={styles.friendItem}>
+      <img src={avatar} alt="Avatar" width="48" className={styles.avatar} />
+      <p className={styles.name}>{name}</p>
+      <p className={isOnline ? styles.online : styles.offline}>
+        {isOnline ? "Online" : "Offline"}
+      </p>
+    </li>
+  );
+};
+
+export default FriendListItem;
